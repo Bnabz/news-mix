98f2ddd6bd794cefed0dce3fc15f75220ead9a1f
Nabz <bnnabiswa@gmail.com>
2020-04-27 23:26:06 +0300

    change get sources function to use urlib.request

--- app/requests.py
+++ app/requests.py
@@ -6,14 +6,26 @@ from .models import Article
 api_key = None
 
 def configure_request(app):
-	global api_key,base_url,articles_url
+	global api_key,sources_url,articles_url
 	api_key = app.config('NEWS_API_KEY')
 	sources_url = app.config['NEWS_SOURCES_BASE_URL']
 	articles_url = app.config['ARTICLES_BASE_URL']
 
 def get_sources():
-    get_news_sources_url = news_sources_url.format(api_key)
-    get_sources_response = requests.get(get_news_sources_url).json()
+    get_news_sources_url = sources_url.format(api_key)
 
-    return get_sources_response["sources"]
+
+    with urllib.request.urlopen(get_news_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources = None
+
+        if get_sources_response['sources']:
+            sources = get_sources_response['sources']
+            
+    return sources
+
+
+    
 
