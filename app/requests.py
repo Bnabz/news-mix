8395eb421f9af011cd76a8aec558b1c0dc2e9e46
Nabz <bnnabiswa@gmail.com>
2020-04-27 19:07:07 +0300

    create get sources function

--- app/requests.py
+++ app/requests.py
@@ -1,20 +1,19 @@
-import requests
+import urllib.request,json
+from .models import Article
 
-from config import Config
+
 
 api_key = None
 
 def configure_request(app):
-    global api_key,base_url
-    api_key = app.config['MOVIE_API_KEY']
-    base_url = app.config['MOVIE_API_BASE_URL']
+	global api_key,base_url,articles_url
+	api_key = app.config('NEWS_API_KEY')
+	sources_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_url = app.config['ARTICLES_BASE_URL']
 
-def get_sources(self):
-    sources = []
-    sources_url = 'https://newsapi.org/v2/sources?q={}&apiKey={}'.format(sources, self.API_KEY)
-    response = requests.get(sources_url)
-    if response.status_code == 200:
-        for data in response.json()['sources']:
-            sources.append(data)
-        print(sources)
-        return sources
+def get_sources():
+    get_news_sources_url = news_sources_url.format(api_key)
+    get_sources_response = requests.get(get_news_sources_url).json()
+
+    return get_sources_response["sources"]
+
