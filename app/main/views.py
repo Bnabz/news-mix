3496e242ccaa25a69782349c403332be7c3721ac
Nabz <bnnabiswa@gmail.com>
2020-04-27 19:17:03 +0300

    add index.html route

---
+++ app/main/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources
+
+@main.route('/')
+def index():
+    sources = get_sources
+    if sources:
+        return render_template('index.html', sources=sources)
