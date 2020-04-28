ad49f7bc87c140b57e836f7174161869052cbd62
Nabz <bnnabiswa@gmail.com>
2020-04-28 03:15:16 +0300

    integreate newsApi in requests

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..requests import get_sources
 
 @main.route('/')
 def index():
-    sources = get_sources
+    sources = get_sources()
     print(sources)
     if sources:
         return render_template('index.html', sources=sources)
--- app/requests.py
+++ app/requests.py
@@ -1,6 +1,7 @@
 import urllib.request,json
 from .models import Article
 import requests
+from newsapi import NewsApiClient
 
 
 
@@ -13,6 +14,8 @@ def configure_request(app):
 	api_key = app.config['NEWS_API_KEY']
 	# sources_url = app.config['NEWS_SOURCES_BASE_URL']
 	# articles_url = app.config['ARTICLES_BASE_URL']
+
+newsapi = NewsApiClient(api_key='4d2a9b72117b4ae0a28523b8a5a092e2')
 
 # def get_sources():
 #     get_news_sources_url = sources_url.format(api_key)
@@ -29,15 +32,19 @@ def configure_request(app):
             
 #     return sources
 
-def get_sources(self):
+def get_sources():
     sources = []
     sources_url = 'https://newsapi.org/v2/sources?q={}&apiKey={}'.format(sources, api_key)
     response = requests.get(sources_url)
     if response.status_code == 200:
         for data in response.json()['sources']:
             sources.append(data)
-        print(sources)
     return sources
+
+# def get_sources():
+#     sources = newsapi.get_sources()
+#     print("hello")
+#     return sources
 
 
     
