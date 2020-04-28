b3c15a1a909711664188fa5bb224102c2e72884b
Nabz <bnnabiswa@gmail.com>
2020-04-28 03:35:02 +0300

    add  cards for sources

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,5 @@ from ..requests import get_sources
 @main.route('/')
 def index():
     sources = get_sources()
-    print(sources)
     if sources:
         return render_template('index.html', sources=sources)
